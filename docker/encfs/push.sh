6b743b5906929763479bf5f6e48e147189676b8f
Dominic Ayre <dominicayre@microsoft.com>
2023-07-18 13:03:29 +0100

    Create workflows for pushing SKR and EncFS images (#7)
    To prevent the attacks like the one suffered from the old repo, this workflow will only run on PRs from branches not from a fork.
    
    Also included a check to skip the run in the case of a fork PR

--- docker/encfs/push.sh
+++ docker/encfs/push.sh
@@ -10,7 +10,10 @@ set -e
 # $1 remote registry name (e.g., myregistry)
 # $2 remote registry domain (e.g., azurecr.io)
 # $3 container_name:container:version (e.g., encfs:1.1)
+# $4 --skip-login flag
 
-az acr login --name $1 
+if ! [ -n "$4" ] || [ "$4" != "--skip-login" ]; then
+    az acr login --name $1 
+fi
 docker tag encfs $1.$2/$3
 docker push $1.$2/$3
