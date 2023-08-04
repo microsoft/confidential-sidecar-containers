f266145facc0bc536cbf34aed02f2864142f6b88
Heather Garvison <hgarvison@microsoft.com>
2023-08-04 12:33:30 -0400

    adding debug dockerfile and build scripts to copy log to the shared fs for testing

---
+++ docker/encfs/encfs-debug.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Important note: This script is meant to run from inside the container
+
+if [[ -z "${EncfsSideCarArgs}" ]]; then
+  EncfsSideCarArgs=$1
+fi
+
+echo EncfsSideCarArgs = $EncfsSideCarArgs
+
+if [[ -z "${EncfsSideCarArgs}" ]]; then
+  if /bin/remotefs -logfile /log.txt; then
+    echo "1" > result
+  else
+    echo "0" > result
+  fi
+else
+  if /bin/remotefs -logfile /log.txt -base64 $EncfsSideCarArgs; then
+    echo "1" > result
+  else
+    echo "0" > result
+  fi
+fi
+
+# Wait forever and copy the log file to the share
+while true; do cp /log.txt /mnt/remote/share/log.txt; sleep 1; done
