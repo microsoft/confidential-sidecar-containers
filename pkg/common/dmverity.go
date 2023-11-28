752309bf8cd800defb26c0a4881b81d1a896bbf1
Jinnan Guo <jinnan.guo@imperial.ac.uk>
2023-11-28 17:47:49 +0000

    Fix typo

--- pkg/common/dmverity.go
+++ pkg/common/dmverity.go
@@ -2,7 +2,7 @@ package common
 
 // DmVerity contains information about the hash device and dm-verity root hash to open the encrypted fs with dm-verity protection
 type DmVerity struct {
-	Enable bool `json:"enable,omitempty`
-	HashUrl string `json:"hash_url,omitemtpy`
-	RootHash string `json:"root_hash,omitemtpy`
+	Enable bool `json:"enable,omitempty"`
+	HashUrl string `json:"hash_url,omitemtpy"`
+	RootHash string `json:"root_hash,omitemtpy"`
 }
