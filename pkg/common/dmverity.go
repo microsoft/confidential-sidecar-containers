8ef50d5ee66b8bf524bd790c7fa10993a8bfc61d
Jinnan Guo <jinnan.guo@imperial.ac.uk>
2023-11-28 14:47:44 +0000

    Add dm-verity info parsing

---
+++ pkg/common/dmverity.go
@@ -0,0 +1,8 @@
+package common
+
+// DmVerity contains information about the hash device and dm-verity root hash to open the encrypted fs with dm-verity protection
+type DmVerity struct {
+	Enable bool `json:"enable,omitempty`
+	HashUrl string `json:"hash_url,omitemtpy`
+	RootHash string `json:"root_hash,omitemtpy`
+}
