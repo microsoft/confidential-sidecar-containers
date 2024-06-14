02cd7c47585c8d2f7ae6777b9ae68973dba5ba05
Dominic Ayre <dominicayre@microsoft.com>
2024-06-14 07:53:47 +0000

    Catch failure in installing c_aci_testing package

--- scripts/install-c-aci-testing.sh
+++ scripts/install-c-aci-testing.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 latest=$(gh release list -R microsoft/confidential-aci-testing -L 1 --json tagName --jq '.[0].tagName')
 gh release download $latest -R microsoft/confidential-aci-testing
 python -m pip install c-aci-testing*.tar.gz
