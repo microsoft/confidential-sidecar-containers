e9d88ff6c7171070223c187a688e339e77eb31be
Dominic Ayre <domayre@outlook.com>
2024-07-15 16:17:42 +0100

    Pin Az CLI version

---
+++ scripts/install-az-cli.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+
+AZ_DIST=$(lsb_release -cs)
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/azure-cli/
+Suites: ${AZ_DIST}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
+
+sudo apt-get install azure-cli=${AZ_CLI_VERSION:-2.61.0}-1~${AZ_DIST}
