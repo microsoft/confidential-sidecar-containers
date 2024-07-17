ff4eb3b9ecfbcd79fba6b65be411392ccf526dc9
Dominic Ayre <dominicayre@microsoft.com>
2024-07-17 15:49:01 +0000

    .

--- tests/encfs/test.py
+++ tests/encfs/test.py
@@ -72,7 +72,7 @@ class EncFSTest(unittest.TestCase):
 
             aci_param_set(
                 target_path=target_dir,
-                parameters=f"sidecarArgsB64=" + "'" + base64.urlsafe_b64encode(json.dumps({
+                parameters="sidecarArgsB64=" + "'" + base64.urlsafe_b64encode(json.dumps({
                     "azure_filesystems": [
                         {
                             "mount_point": f"{mount_point}/{blob_id}",
@@ -91,7 +91,7 @@ class EncFSTest(unittest.TestCase):
                         } for blob_id, blob_type in cls.blobs
                     ]
                 }).encode()).decode() + "'",
-                )
+            )
 
             images_build(**image_args)
             images_push(**image_args)
