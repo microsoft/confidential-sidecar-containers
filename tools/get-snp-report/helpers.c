3d590afde554b4608fc85d837f649858e357a944
Tingmao Wang <tingmaowang@microsoft.com>
2025-03-04 17:51:28 +0000

    Update report structure in c code according to latest spec

--- tools/get-snp-report/helpers.c
+++ tools/get-snp-report/helpers.c
@@ -16,36 +16,37 @@
 
 // Helper functions
 uint8_t* decodeHexString(const char *hexstring, size_t padTo) // will zero pad to bufferLen
-{   
+{
     size_t len = strlen(hexstring);
     size_t out_len = len/2+1;
     if (out_len < padTo)
         out_len = padTo;
     uint8_t *byte_array = (uint8_t*) malloc(out_len);
     memset(byte_array, 0, out_len);
-    
-    for (size_t i = 0; i < len; i+=2) {        
+
+    for (size_t i = 0; i < len; i+=2) {
         sscanf(hexstring, "%2hhx", &byte_array[i/2]);
-        hexstring += 2;        
+        hexstring += 2;
     }
 
     return byte_array;
 }
 
 char* encodeHexToString(uint8_t byte_array[], size_t len)
-{    
+{
     char* hexstring = (char*) malloc((2*len+1)*sizeof(char));
 
-    for (size_t i = 0; i < len; i++)       
-        sprintf(&hexstring[i*2], "%02x", byte_array[i]);                    
-    
-    hexstring[2*len] = '\0'; // string padding character    
+    for (size_t i = 0; i < len; i++)
+        sprintf(&hexstring[i*2], "%02x", byte_array[i]);
+
+    hexstring[2*len] = '\0'; // string padding character
     return hexstring;
 }
 
 void printBytes(const char *desc, const uint8_t *data, size_t len, bool swap)
 {
     fprintf(stdout, "  %s: ", desc);
+    /* Pad it so that we have 24 characters before the hex */
     int padding = 20 - strlen(desc);
     if (padding < 0)
         padding = 0;
@@ -62,8 +63,17 @@ void printBytes(const char *desc, const uint8_t *data, size_t len, bool swap)
     fprintf(stdout, "\n");
 }
 
+void checkAllZeros(const uint8_t *data, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        if (data[i] != 0) {
+            fprintf(stdout, "%24s! reserved field not zero\n", "");
+            return;
+        }
+    }
+}
+
 void printReport(const snp_attestation_report *r)
-{    
+{
     PRINT_VAL(r, version);
     PRINT_VAL(r, guest_svn);
     PRINT_VAL(r, policy);
@@ -71,10 +81,10 @@ void printReport(const snp_attestation_report *r)
     PRINT_VAL(r, image_id);
     PRINT_VAL(r, vmpl);
     PRINT_VAL(r, signature_algo);
-    PRINT_BYTES(r, platform_version);
+    PRINT_BYTES(r, current_tcb);
     PRINT_BYTES(r, platform_info);
-    PRINT_VAL(r, author_key_en);
-    PRINT_VAL(r, reserved1);
+    PRINT_BYTES(r, author_key_en);
+    PRINT_RESERVED(r, _reserved2);
     PRINT_BYTES(r, report_data);
     PRINT_BYTES(r, measurement);
     PRINT_BYTES(r, host_data);
@@ -83,8 +93,21 @@ void printReport(const snp_attestation_report *r)
     PRINT_BYTES(r, report_id);
     PRINT_BYTES(r, report_id_ma);
     PRINT_VAL(r, reported_tcb);
-    PRINT_BYTES(r, reserved2);
+    PRINT_VAL(r, cpuid_fam_id);
+    PRINT_VAL(r, cpuid_mod_id);
+    PRINT_VAL(r, cpuid_step);
+    PRINT_RESERVED(r, _reserved3);
     PRINT_BYTES(r, chip_id);
-    PRINT_BYTES(r, reserved3);
+    PRINT_BYTES(r, committed_tcb);
+    PRINT_VAL(r, current_build);
+    PRINT_VAL(r, current_minor);
+    PRINT_VAL(r, current_major);
+    PRINT_RESERVED(r, _reserved4);
+    PRINT_VAL(r, committed_build);
+    PRINT_VAL(r, committed_minor);
+    PRINT_VAL(r, committed_major);
+    PRINT_RESERVED(r, _reserved5);
+    PRINT_BYTES(r, launch_tcb);
+    PRINT_RESERVED(r, _reserved6);
     PRINT_BYTES(r, signature);
 }
--- tools/get-snp-report/helpers.h
+++ tools/get-snp-report/helpers.h
@@ -18,6 +18,10 @@
 
 #define PRINT_VAL(ptr, field) printBytes(#field, (const uint8_t *)&(ptr->field), sizeof(ptr->field), true)
 #define PRINT_BYTES(ptr, field) printBytes(#field, (const uint8_t *)&(ptr->field), sizeof(ptr->field), false)
+#define PRINT_RESERVED(ptr, field) do { \
+   printBytes(#field, (const uint8_t *)&(ptr->field), sizeof(ptr->field), false); \
+   checkAllZeros((const uint8_t *)&(ptr->field), sizeof(ptr->field)); \
+} while (0)
 
 // Helper functions
 uint8_t* decodeHexString(const char *hexstring, size_t padTo);
@@ -25,5 +29,7 @@ uint8_t* decodeHexString(const char *hexstring, size_t padTo);
 char* encodeHexToString(uint8_t byte_array[], size_t len);
 
 void printBytes(const char *desc, const uint8_t *data, size_t len, bool swap);
+
+void checkAllZeros(const uint8_t *data, size_t len);
 
 void printReport(const snp_attestation_report *r);
